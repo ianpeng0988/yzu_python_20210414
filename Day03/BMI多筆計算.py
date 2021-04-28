32add63706dd45df8b02d040b114b413cf7ce3ea
ianpeng0988 <anotherswear@gmail.com>
2021-04-28 20:28:03 +0800

    python

--- Day03/BMI多筆計算.py
+++ Day03/BMI多筆計算.py
@@ -7,7 +7,9 @@ import math
 '''
 def printBMI(h, w):
     bmi = w / math.pow(h/100, 2)
-    print("h= %.1f w=%.1f bmi=%.2f" % (h, w, bmi))
+    result = "過重" if bmi > 23 else "過輕" if bmi <= 18 else "正常"
+    print("h= %.1f w=%.1f bmi=%.2f result=%s" % (h, w, bmi, result))
+
 
 printBMI(170, 60)
 printBMI(180, 70)
