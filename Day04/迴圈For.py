1c34915f37d79fee8cce5671dd4ebf562a141956
ianpeng0988 <anotherswear@gmail.com>
2021-05-05 21:11:50 +0800

    python

--- Day04/迴圈For.py
+++ Day04/迴圈For.py
@@ -1,2 +1,8 @@
-for i in range(1, 5):
-    print('Java')
+# for-loop (步進迴圈)
+for i in range(1, 10, 2):
+    print(i, 'Java')
+
+# for-in  (遍歷迴圈)
+for score in [100, 90, 50, 70]:
+    if score >= 60:
+        print(score)
