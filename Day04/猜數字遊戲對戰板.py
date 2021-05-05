33f968bd61163af9faff4f335b5979ed711dc357
ianpeng0988 <anotherswear@gmail.com>
2021-05-05 20:57:40 +0800

    python

--- Day04/猜數字遊戲對戰板.py
+++ Day04/猜數字遊戲對戰板.py
@@ -4,18 +4,35 @@ min = 0
 max = 100
 count = 5
 while count > 0:
+    #  使用者猜
     guess = int(input)('(%d). 請輸入 %d ~ %d : ' % (count, min, max))
     #  檢查guess 的資料是否在 min 與 max 之間 ?
     if guess <= min or guess >= max:
         print('數字範圍錯誤')
         continue
-    #  將count 減去一次
-    count = count - 1
+
     #  判定結果
-    if guess > ans:
+def chenk(guess, who):
+    global min, max
+    if check(guess > ans:
         max = guess
     elif guess < ans:
         min = guess
     else:
-        print('答對了')
+        print('使用者答對了')
         break
+
+    # 電腦猜
+guess = r.randint(min+1, max-1)
+print('(%d). 電腦猜 %d ~ %d :%d ' % (count, min, max, guess))
+#  判定結果
+if guess > ans:
+    max = guess
+elif guess < ans:
+    min = guess
+else:
+    print('電腦答對了')
+
+
+#  將count 減去一次
+    count = count - 1
