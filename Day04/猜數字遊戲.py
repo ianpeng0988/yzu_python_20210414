f10a2cffcbca832dfe80ffd229ce134963b8b2e1
ianpeng0988 <anotherswear@gmail.com>
2021-05-05 19:59:28 +0800

    python

--- Day04/猜數字遊戲.py
+++ Day04/猜數字遊戲.py
@@ -5,10 +5,15 @@ max = 100
 
 while True:
     guess = int(input('請輸入 %d ~ %d : ' % (min, max)))
+    #  檢查guess 的資料是否在 min 與 max 之間 ?
+    if guess <= min or guess >= max:
+        print('數字範圍錯誤')
+        continue
+    #  判定結果
     if guess > ans:
         max = guess
     elif guess < ans:
         min = guess
     else:
         print('答對了')
-        break;
+        break
